0b9cfcb047900a0a653f0398b43ba9d400802d2b
KSHATest <sklyzet@yandex.ru>
2024-03-04 10:00:14 +0500

    bonusmiles

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-        if (price <= 1000) {
-            return 0;
-        } else {
-            int BonusMiles = price / 20;
-            return (BonusMiles);
-        }
+            int bonusmiles = price / 20;
+            return (bonusmiles);
+
     }
 }
