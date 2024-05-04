e14cabc7b6c85234bef0781c7a1c86440b48795d
KSHATest <sklyzet@yandex.ru>
2024-05-04 09:22:36 +0500

    formatting

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-            int bonusmiles = price / 20;
-            return (bonusmiles);
+        int BonusMiles = price / 20;
+        return (BonusMiles);
 
     }
 }
